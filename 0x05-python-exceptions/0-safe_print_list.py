ac06f1de5564dc26c516e0f56a59aa2f8fc474bf
Chinecherem <chinecheremubawike@gmail.com>
2024-04-02 17:18:05 +0100

    New changes

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list, x):
 		count = 0
 		for element in my_list:
 			if (count < x):
-				print(element, end=' ')
+				print(element, end='')
 				count += 1
 		print('')
 		return count
